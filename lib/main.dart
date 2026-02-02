8fa6c00ca22f437095c1b4f4212e724aaa930afd
Md Aamir <mdcreator.2.0@gmail.com>
2026-02-03 02:35:50 +0530

    feat: Implement responsive navigation rail using `LayoutBuilder` and refactor page selection logic with a `switch` statement.

--- lib/main.dart
+++ lib/main.dart
@@ -52,40 +52,54 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Row(
-        children: [
-          SafeArea(
-            child: NavigationRail(
-              extended: false,
-              destinations: [
-                NavigationRailDestination(
-                  icon: Icon(Icons.home),
-                  label: Text('Home'),
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = GeneratorPage();
+      case 1:
+        page = Placeholder();
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+    }
+
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Scaffold(
+          body: Row(
+            children: [
+              SafeArea(
+                child: NavigationRail(
+                  extended: constraints.maxWidth >= 600,
+                  destinations: [
+                    NavigationRailDestination(
+                      icon: Icon(Icons.home),
+                      label: Text('Home'),
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.favorite),
+                      label: Text('Favorites'),
+                    ),
+                  ],
+                  selectedIndex: selectedIndex,
+                  onDestinationSelected: (value) {
+                    setState(() {
+                      selectedIndex = value;
+                    });
+                  },
                 ),
-                NavigationRailDestination(
-                  icon: Icon(Icons.favorite),
-                  label: Text('Favorites'),
+              ),
+              Expanded(
+                child: Container(
+                  color: selectedIndex == 0
+                      ? Theme.of(context).colorScheme.primaryContainer
+                      : Colors.amber,
+                  child: page,
                 ),
-              ],
-              selectedIndex: selectedIndex,
-              onDestinationSelected: (value) {
-                setState(() {
-                  selectedIndex = value;
-                });
-              },
-            ),
+              ),
+            ],
           ),
-          Expanded(
-            child: Container(
-              color: selectedIndex == 0
-                  ? Theme.of(context).colorScheme.primaryContainer
-                  : Colors.amber,
-              child: selectedIndex == 0 ? GeneratorPage() : FavoritesPage(),
-            ),
-          ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
